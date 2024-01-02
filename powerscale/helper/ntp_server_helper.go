027bdc093060df570b65c0e1a2f8c8cce3f49759
Yuhao Liu <Yuhao_Liu@dell.com>
2024-01-02 15:22:29 +0800

    NTP Server Resource Implementation

---
+++ powerscale/helper/ntp_server_helper.go
@@ -0,0 +1,59 @@
+/*
+Copyright (c) 2023 Dell Inc., or its subsidiaries. All Rights Reserved.
+
+Licensed under the Mozilla Public License Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://mozilla.org/MPL/2.0/
+
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package helper
+
+import (
+	"context"
+	powerscale "dell/powerscale-go-client"
+	"terraform-provider-powerscale/client"
+	"terraform-provider-powerscale/powerscale/models"
+)
+
+// CreateNtpServer Create a NTP Server.
+func CreateNtpServer(ctx context.Context, client *client.Client, ntpServer powerscale.V3NtpServer) (string, error) {
+	ntpID, _, err := client.PscaleOpenAPIClient.ProtocolsApi.CreateProtocolsv3NtpServer(ctx).V3NtpServer(ntpServer).Execute()
+	if id, ok := ntpID["id"].(string); ok {
+		return id, err
+	}
+	return "", err
+}
+
+// GetNtpServer retrieve NTP Server information.
+func GetNtpServer(ctx context.Context, client *client.Client, ntpServerModel models.NtpServerResourceModel) (*powerscale.V3NtpServersExtended, error) {
+	queryParam := client.PscaleOpenAPIClient.ProtocolsApi.GetProtocolsv3NtpServer(ctx, ntpServerModel.ID.ValueString())
+	ntpServerRes, _, err := queryParam.Execute()
+	return ntpServerRes, err
+}
+
+// UpdateNtpServer Update a NTP Server.
+func UpdateNtpServer(ctx context.Context, client *client.Client, ntpServerID string, ntpServerToUpdate powerscale.V3NtpServerExtendedExtended) error {
+	updateParam := client.PscaleOpenAPIClient.ProtocolsApi.UpdateProtocolsv3NtpServer(ctx, ntpServerID)
+	_, err := updateParam.V3NtpServer(ntpServerToUpdate).Execute()
+	return err
+}
+
+// DeleteNtpServer Delete a NTP Server.
+func DeleteNtpServer(ctx context.Context, client *client.Client, ntpServerID string) error {
+	_, err := client.PscaleOpenAPIClient.ProtocolsApi.DeleteProtocolsv3NtpServer(ctx, ntpServerID).Execute()
+	return err
+}
+
+// IsUpdateNtpServerParamInvalid Verify if update params contain params only for creating.
+func IsUpdateNtpServerParamInvalid(plan models.NtpServerResourceModel, state models.NtpServerResourceModel) bool {
+	return !state.Name.Equal(plan.Name)
+}
