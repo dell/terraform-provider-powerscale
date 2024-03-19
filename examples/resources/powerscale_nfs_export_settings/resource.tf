72da5f4d895b6caa72ebec6b3a76b0ce564e3089
Jason_Jin1 <Jason_jin1@dell.com>
2024-03-19 22:08:40 +0800

    Implement NFS Export Settings Resource

---
+++ examples/resources/powerscale_nfs_export_settings/resource.tf
@@ -0,0 +1,46 @@
+/*
+Copyright (c) 2024 Dell Inc., or its subsidiaries. All Rights Reserved.
+
+Licensed under the Mozilla Public License Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://mozilla.org/MPL/2.0/
+
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+# Available actions: Create, Update, Delete and Import.
+# If resource arguments are omitted, `terraform apply` will load NFS export settings from PowerScale, and save to terraform state file.
+# If any resource arguments are specified, `terraform apply` will try to load NFS export settings (if not loaded) and update the settings.
+# `terraform destroy` will delete the resource from terraform state file rather than deleting NFS export settings from PowerScale.
+# For more information, Please check the terraform state file.
+
+# PowerScale NFS Export Settings allow you to configure NFS export settings on PowerScale.
+resource "powerscale_nfs_export_settings" "example" {
+  # Optional fields both for creating and updating
+  #  all_dirs = false
+  #  case_insensitive = false
+  #  case_preserving = true
+  #  commit_asynchronous = false
+  #  no_truncate = false
+  #  write_datasync_action = "DATASYNC"
+  #  security_flavors = ["unix"]
+  #  map_non_root = {
+  #    enabled = false
+  #    primary_group = {}
+  #    secondary_groups = []
+  #    user = {
+  #      id = "USER:nobody"
+  #    }
+  #  }
+}
+
+# After the execution of above resource block, NFS export settings would have been cached in terraform state file, or
+# NFS export settings would have been updated on PowerScale.
+# For more information, Please check the terraform state file.
