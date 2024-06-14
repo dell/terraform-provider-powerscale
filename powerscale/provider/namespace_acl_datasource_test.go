db8f98d93e64d152503c46c96239235b6f7a7151
MangirishK <44159729+MangirishK@users.noreply.github.com>
2024-06-14 05:56:46 +0530

    Implement copyright check (#170)

--- powerscale/provider/namespace_acl_datasource_test.go
+++ powerscale/provider/namespace_acl_datasource_test.go
@@ -1,11 +1,11 @@
 /*
 Copyright (c) 2024 Dell Inc., or its subsidiaries. All Rights Reserved.
 
-Licensed under the Mozilla Public License Version 2 (the "License");
+Licensed under the Mozilla Public License Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://mozilla.org/MPL/2/
+    http://mozilla.org/MPL/2.0/
 
 
 Unless required by applicable law or agreed to in writing, software
