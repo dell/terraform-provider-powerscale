d1052beb2e2e35475eb684f57452a6b064e5f49c
Rounak Adhikary <Rounak.Adhikary@dell.com>
2024-08-19 05:54:16 -0400

    Sec changes

--- powerscale/helper/synciq_policy.go
+++ powerscale/helper/synciq_policy.go
@@ -1,3 +1,20 @@
+/*
+Copyright (c) 2023-2024 Dell Inc., or its subsidiaries. All Rights Reserved.
+
+Licensed under the Mozilla Public License Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://mozilla.org/MPL/2.0/
+
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package helper
 
 import (
@@ -34,9 +51,9 @@ func NewSyncIQPolicyDataSource[V SyncIQPolicyDataSourceResponse](ctx context.Con
 		ID:       types.StringValue("dummy"),
 		Policies: make([]models.V14SyncPolicyExtendedModel, len(policies)),
 	}
-	for i, source := range policies {
+	for i := range policies {
 		var item models.V14SyncPolicyExtendedModel
-		ierr := CopyFields(ctx, &source, &item)
+		ierr := CopyFields(ctx, &policies[i], &item)
 		err = errors.Join(err, ierr)
 		ret.Policies[i] = item
 	}
