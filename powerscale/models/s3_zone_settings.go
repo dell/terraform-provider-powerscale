9bfaf32fba94b61de2f60dd72d57409f8f439067
Priyanshu Krishnan <priyanshu.krishnan@dell.com>
2024-08-26 01:30:23 -0400

    fixed linting

--- powerscale/models/s3_zone_settings.go
+++ powerscale/models/s3_zone_settings.go
@@ -2,10 +2,11 @@ package models
 
 import "github.com/hashicorp/terraform-plugin-framework/types"
 
+// S3ZoneSettingsResource defines the resource arguments.
 type S3ZoneSettingsResource struct {
 	Zone                      types.String `tfsdk:"zone"`
 	BaseDomain                types.String `tfsdk:"base_domain"`
 	BucketDirectoryCreateMode types.Int64  `tfsdk:"bucket_directory_create_mode"`
-	ObjectAclPolicy           types.String `tfsdk:"object_acl_policy"`
+	ObjectACLPolicy           types.String `tfsdk:"object_acl_policy"`
 	RootPath                  types.String `tfsdk:"root_path"`
 }
