4f0ff01be0b6300c315da7e7c62665d2015bb196
Sakshi_makkar <Sakshi_makkar@dell.com>
2024-08-27 05:08:38 -0400

    Cluster Identity Resource added

---
+++ powerscale/helper/cluster_identity_helper.go
@@ -0,0 +1,105 @@
+/*
+Copyright (c) 2023-2024 Dell Inc., or its subsidiaries. All Rights Reserved.
+
+Licensed under the Mozilla Public License Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://mozilla.org/MPL/2.0/
+
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package helper
+
+import (
+	"context"
+	powerscale "dell/powerscale-go-client"
+	"fmt"
+	"terraform-provider-powerscale/client"
+	"terraform-provider-powerscale/powerscale/constants"
+	"terraform-provider-powerscale/powerscale/models"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+// UpdateClusterIdentity calls the API to update the cluster identity.
+func UpdateClusterIdentity(ctx context.Context, client *client.Client, clusterIdentity powerscale.V3ClusterIdentity) error {
+	_, err := client.PscaleOpenAPIClient.ClusterApi.UpdateClusterv3ClusterIdentity(ctx).V3ClusterIdentity(clusterIdentity).Execute()
+	return err
+}
+
+// ManageClusterIdentity reads and updates the cluster identity.
+func ManageClusterIdentity(ctx context.Context, plan models.ClusterIdentityResource, state *models.ClusterIdentityResource, client *client.Client) diag.Diagnostics {
+	var toUpdate powerscale.V3ClusterIdentity
+	var diags diag.Diagnostics
+	err := ReadFromState(ctx, &plan, &toUpdate)
+	if err != nil {
+		errStr := constants.UpdateClusterIdentitySettingsErrorMsg + "with error: "
+		message := GetErrorString(err, errStr)
+		diags.AddError(
+			"Error updating cluster identity",
+			fmt.Sprintf("Could not read cluster identity param with error: %s", message),
+		)
+		return diags
+	}
+
+	err = UpdateClusterIdentity(ctx, client, toUpdate)
+	if err != nil {
+		errStr := constants.UpdateClusterIdentitySettingsErrorMsg + "with error: "
+		message := GetErrorString(err, errStr)
+		diags.AddError(
+			"Error updating cluster identity",
+			message,
+		)
+		return diags
+	}
+
+	clusterIdentity, err := GetClusterIdentity(ctx, client)
+	if err != nil {
+		errStr := constants.ReadClusterIdentitySettingsErrorMsg + "with error: "
+		message := GetErrorString(err, errStr)
+		diags.AddError(
+			"Error fetching updated cluster identity settings",
+			message,
+		)
+		return diags
+	}
+	UpdateClusterIdentityState(state, clusterIdentity)
+
+	return diags
+}
+
+// UpdateClusterIdentityState is a function to update cluster identity state.
+func UpdateClusterIdentityState(state *models.ClusterIdentityResource, clusterIdentity *powerscale.V1ClusterIdentity) diag.Diagnostics {
+	mappingLogonObject2Type := map[string]attr.Type{
+		"motd":        types.StringType,
+		"motd_header": types.StringType,
+	}
+
+	LogonElemMap := map[string]attr.Value{
+		"motd":        types.StringValue(clusterIdentity.Logon.Motd),
+		"motd_header": types.StringValue(clusterIdentity.Logon.MotdHeader),
+	}
+
+	lookuplogonObject, diags := types.ObjectValue(mappingLogonObject2Type, LogonElemMap)
+	if diags.HasError() {
+		diags.AddError(
+			"Unable to update cluster identity state",
+			"Unmarshalling logon object failed",
+		)
+		return diags
+	}
+	state.Logon = lookuplogonObject
+	state.Name = types.StringValue(clusterIdentity.Name)
+	state.Description = types.StringValue(clusterIdentity.Description)
+	state.ID = types.StringValue("cluster_identity")
+	return nil
+}
---
+++ powerscale/provider/cluster_identity_resource_test.go
@@ -0,0 +1,320 @@
+/*
+Copyright (c) 2023-2024 Dell Inc., or its subsidiaries. All Rights Reserved.
+
+Licensed under the Mozilla Public License Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://mozilla.org/MPL/2.0/
+
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package provider
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+	"terraform-provider-powerscale/client"
+	"terraform-provider-powerscale/powerscale/helper"
+	"testing"
+
+	"github.com/bytedance/mockey"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+)
+
+func TestAccClusterIdentityResourceImport(t *testing.T) {
+	var clusterIdentityResourceName = "powerscale_cluster_identity.test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create Success
+			{
+				Config: ProviderConfig + clusterIdentityResourceConfig,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(clusterIdentityResourceName, "name", "cluster1"),
+				),
+			},
+			{
+				PreConfig: func() {
+					if FunctionMocker != nil {
+						FunctionMocker.Release()
+					}
+					FunctionMocker = mockey.Mock(helper.GetClusterIdentity).Return(nil, fmt.Errorf("mock error")).Build()
+				},
+				Config:       ProviderConfig + clusterIdentityResourceConfig,
+				ResourceName: clusterIdentityResourceName,
+				ImportState:  true,
+				ExpectError:  regexp.MustCompile(`.*mock error*.`),
+			},
+			// Import testing
+			{
+				PreConfig: func() {
+					FunctionMocker.Release()
+				},
+				Config:       ProviderConfig + clusterIdentityResourceConfig,
+				ResourceName: clusterIdentityResourceName,
+				ImportState:  true,
+				ExpectError:  nil,
+				ImportStateCheck: func(s []*terraform.InstanceState) error {
+					resource.TestCheckResourceAttrSet(clusterIdentityResourceName, "id")
+					resource.TestCheckResourceAttrSet(clusterIdentityResourceName, "name")
+					resource.TestCheckResourceAttrSet(clusterIdentityResourceName, "description")
+					return nil
+				},
+			},
+		},
+	})
+}
+
+// TestAccClusterIdentityResource - Tests the creation of a cluster Identity resource.
+func TestAccClusterIdentityResource(t *testing.T) {
+	var clusterIdentityResourceName = "powerscale_cluster_identity.test"
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: ProviderConfig + clusterIdentityResourceConfig,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(clusterIdentityResourceName, "name", "cluster1"),
+				),
+			},
+		},
+	})
+}
+
+// TestAccClusterIdentityResource_Update - Tests the update of a cluster Identity resource along with error mocking.
+func TestAccClusterIdentityResource_Update(t *testing.T) {
+	var clusterIdentityResourceName = "powerscale_cluster_identity.test"
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: ProviderConfig + clusterIdentityResourceConfig,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(clusterIdentityResourceName, "id"),
+					resource.TestCheckResourceAttr(clusterIdentityResourceName, "name", "cluster1"),
+				),
+			},
+			// Read testing
+			{
+				PreConfig: func() {
+					if FunctionMocker != nil {
+						FunctionMocker.Release()
+					}
+					FunctionMocker = mockey.Mock(helper.GetClusterIdentity).Return(nil, fmt.Errorf("mock error")).Build()
+				},
+				Config:      ProviderConfig + clusterIdentityResourceConfig,
+				ExpectError: regexp.MustCompile(`.*mock error*.`),
+			},
+			// Update Mock Error
+			{
+				PreConfig: func() {
+					if FunctionMocker != nil {
+						FunctionMocker.Release()
+					}
+					FunctionMocker = mockey.Mock(helper.ReadFromState).Return(fmt.Errorf("mock error")).Build()
+				},
+				Config:      ProviderConfig + clusterIdentityResourceConfigUpdate,
+				ExpectError: regexp.MustCompile(`.*mock error*.`),
+			},
+			// Update Mock Error
+			{
+				PreConfig: func() {
+					if FunctionMocker != nil {
+						FunctionMocker.Release()
+					}
+					FunctionMocker = mockey.Mock(helper.UpdateClusterIdentity).Return(fmt.Errorf("mock error")).Build()
+				},
+				Config:      ProviderConfig + clusterIdentityResourceConfigUpdate,
+				ExpectError: regexp.MustCompile(`.*mock error*.`),
+			},
+			// Update Mock Error
+			{
+				PreConfig: func() {
+					if FunctionMocker != nil {
+						FunctionMocker.Release()
+					}
+					FunctionMocker = mockey.Mock(helper.GetClusterIdentity).Return(nil, fmt.Errorf("mock error")).Build().
+						When(func(ctx context.Context, client *client.Client) bool {
+							return FunctionMocker.Times() == 2
+						})
+				},
+				Config:      ProviderConfig + clusterIdentityResourceConfigUpdate,
+				ExpectError: regexp.MustCompile(`.*mock error*.`),
+			},
+			// Update and Read testing
+			{
+				PreConfig: func() {
+					if FunctionMocker != nil {
+						FunctionMocker.Release()
+					}
+				},
+				Config: ProviderConfig + clusterIdentityResourceConfigUpdate,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(clusterIdentityResourceName, "name", "cluster2"),
+				),
+			},
+			{
+				Config: ProviderConfig + clusterIdentityResourceConfig,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(clusterIdentityResourceName, "id"),
+					resource.TestCheckResourceAttr(clusterIdentityResourceName, "name", "cluster1"),
+				),
+			},
+		},
+	})
+}
+
+// TestAccClusterIdentityResource_Create - Tests the mock errors during the create operation of the cluster Identity resource.
+func TestAccClusterIdentityResourceCreateMockErr(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				PreConfig: func() {
+					FunctionMocker = mockey.Mock(helper.GetClusterIdentity).Return(nil, fmt.Errorf("mock error")).Build()
+				},
+				Config:      ProviderConfig + clusterIdentityResourceConfigUpdate,
+				ExpectError: regexp.MustCompile(`.*mock error*.`),
+			},
+			{
+				PreConfig: func() {
+					if FunctionMocker != nil {
+						FunctionMocker.Release()
+					}
+					FunctionMocker = mockey.Mock(helper.UpdateClusterIdentity).Return(fmt.Errorf("mock error")).Build()
+				},
+				Config:      ProviderConfig + clusterIdentityResourceConfigUpdate,
+				ExpectError: regexp.MustCompile(`.*mock error*.`),
+			},
+			{
+				PreConfig: func() {
+					if FunctionMocker != nil {
+						FunctionMocker.Release()
+					}
+					FunctionMocker = mockey.Mock(helper.ReadFromState).Return(fmt.Errorf("mock error")).Build()
+				},
+				Config:      ProviderConfig + clusterIdentityResourceConfigUpdate,
+				ExpectError: regexp.MustCompile(`.*mock error*.`),
+			},
+			{
+				PreConfig: func() {
+					if FunctionMocker != nil {
+						FunctionMocker.Release()
+					}
+					FunctionMocker = mockey.Mock(helper.UpdateClusterIdentityState).Return(nil).Build()
+				},
+				Config:      ProviderConfig + clusterIdentityResourceConfig,
+				ExpectError: regexp.MustCompile(`.*Value Conversion*.`),
+			},
+		},
+	})
+}
+
+// TestAccClusterIdentityResource_Update - Tests the mock errors during the update operation of the cluster Identity resource.
+func TestAccClusterIdentityResourceUpdateMockErr(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				PreConfig: func() {
+					FunctionMocker = mockey.Mock(helper.GetClusterIdentity).Return(nil, fmt.Errorf("mock error")).Build()
+				},
+				Config:      ProviderConfig + clusterIdentityResourceConfig,
+				ExpectError: regexp.MustCompile(`.*mock error*.`),
+			},
+			{
+				PreConfig: func() {
+					if FunctionMocker != nil {
+						FunctionMocker.Release()
+					}
+					FunctionMocker = mockey.Mock(helper.UpdateClusterIdentity).Return(fmt.Errorf("mock error")).Build()
+				},
+				Config:      ProviderConfig + clusterIdentityResourceConfig,
+				ExpectError: regexp.MustCompile(`.*mock error*.`),
+			},
+			{
+				PreConfig: func() {
+					if FunctionMocker != nil {
+						FunctionMocker.Release()
+					}
+					FunctionMocker = mockey.Mock(helper.ReadFromState).Return(fmt.Errorf("mock error")).Build()
+				},
+				Config:      ProviderConfig + clusterIdentityResourceConfig,
+				ExpectError: regexp.MustCompile(`.*mock error*.`),
+			},
+		},
+	})
+}
+
+// TestAccClusterIdentityResource_Import - Tests the mock errors during the import of the cluster Identity resource.
+func TestAccClusterIdentityResourceImportMockErr(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: ProviderConfig + clusterIdentityResourceConfig,
+			},
+			// Import and read Error testing
+			{
+				PreConfig: func() {
+					if FunctionMocker != nil {
+						FunctionMocker.Release()
+					}
+					FunctionMocker = mockey.Mock(helper.GetClusterIdentity).Return(nil, fmt.Errorf("mock error")).Build()
+				},
+				Config:            ProviderConfig + clusterIdentityResourceConfig,
+				ResourceName:      "powerscale_cluster_identity.test",
+				ImportState:       true,
+				ExpectError:       regexp.MustCompile(`.*mock error*.`),
+				ImportStateVerify: true,
+			},
+			{
+				PreConfig: func() {
+					if FunctionMocker != nil {
+						FunctionMocker.Release()
+					}
+					FunctionMocker = mockey.Mock(helper.UpdateClusterIdentityState).Return(nil).Build()
+				},
+				Config:            ProviderConfig + clusterIdentityResourceConfig,
+				ResourceName:      "powerscale_cluster_identity.test",
+				ImportState:       true,
+				ExpectError:       regexp.MustCompile(`.*Value Conversion*.`),
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+var clusterIdentityResourceConfig = `
+resource "powerscale_cluster_identity" "test" {
+	name = "cluster1"
+	description = "cluster name"
+	logon= {motd = "motd",motd_header = "motd_header"}
+}
+`
+
+var clusterIdentityResourceConfigUpdate = `
+resource "powerscale_cluster_identity" "test" {
+	name = "cluster2"
+}
+`
